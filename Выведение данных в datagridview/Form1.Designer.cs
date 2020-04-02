2b99f4519f11b5afeb10d7443aabfa3e5a42ff4f
Roman <matveev.romon@mail.ru>
2020-04-03 00:14:26 +0400

    Сделал норм ресайз элементов управления

--- Выведение данных в datagridview/Form1.Designer.cs
+++ Выведение данных в datagridview/Form1.Designer.cs
@@ -39,28 +39,14 @@ namespace Выведение_данных_в_datagridview
             this.toolStripMenuItemOpen = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItemClose = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItemAutorun = new System.Windows.Forms.ToolStripMenuItem();
-            this.noteBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.noteBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
-            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
-            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
-            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
-            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
-            this.noteBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
             this.dataGridViewNotes = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.noteBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.contextMenuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.noteBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.noteBindingNavigator)).BeginInit();
-            this.noteBindingNavigator.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNotes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noteBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // timer1
@@ -71,10 +57,12 @@ namespace Выведение_данных_в_datagridview
             // 
             // buttonCreateNote
             // 
+            this.buttonCreateNote.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonCreateNote.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.buttonCreateNote.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
-            this.buttonCreateNote.Location = new System.Drawing.Point(12, 486);
+            this.buttonCreateNote.Location = new System.Drawing.Point(0, 460);
             this.buttonCreateNote.Name = "buttonCreateNote";
-            this.buttonCreateNote.Size = new System.Drawing.Size(609, 80);
+            this.buttonCreateNote.Size = new System.Drawing.Size(707, 84);
             this.buttonCreateNote.TabIndex = 1;
             this.buttonCreateNote.Text = "Создать";
             this.buttonCreateNote.UseVisualStyleBackColor = true;
@@ -121,145 +109,19 @@ namespace Выведение_данных_в_datagridview
             this.toolStripMenuItemAutorun.Text = "Автозапуск";
             this.toolStripMenuItemAutorun.Click += new System.EventHandler(this.toolStripMenuItemAutorun_Click);
             // 
-            // noteBindingSource
-            // 
-            this.noteBindingSource.DataSource = typeof(Выведение_данных_в_datagridview.Note);
-            // 
-            // noteBindingNavigator
-            // 
-            this.noteBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
-            this.noteBindingNavigator.BindingSource = this.noteBindingSource;
-            this.noteBindingNavigator.CountItem = this.bindingNavigatorCountItem;
-            this.noteBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
-            this.noteBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.bindingNavigatorMoveFirstItem,
-            this.bindingNavigatorMovePreviousItem,
-            this.bindingNavigatorSeparator,
-            this.bindingNavigatorPositionItem,
-            this.bindingNavigatorCountItem,
-            this.bindingNavigatorSeparator1,
-            this.bindingNavigatorMoveNextItem,
-            this.bindingNavigatorMoveLastItem,
-            this.bindingNavigatorSeparator2,
-            this.bindingNavigatorAddNewItem,
-            this.bindingNavigatorDeleteItem,
-            this.noteBindingNavigatorSaveItem});
-            this.noteBindingNavigator.Location = new System.Drawing.Point(0, 0);
-            this.noteBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
-            this.noteBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
-            this.noteBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
-            this.noteBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
-            this.noteBindingNavigator.Name = "noteBindingNavigator";
-            this.noteBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
-            this.noteBindingNavigator.Size = new System.Drawing.Size(632, 25);
-            this.noteBindingNavigator.TabIndex = 2;
-            this.noteBindingNavigator.Text = "bindingNavigator1";
-            // 
-            // bindingNavigatorMoveFirstItem
-            // 
-            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
-            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
-            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorMoveFirstItem.Text = "Переместить в начало";
-            // 
-            // bindingNavigatorMovePreviousItem
-            // 
-            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
-            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
-            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorMovePreviousItem.Text = "Переместить назад";
-            // 
-            // bindingNavigatorSeparator
-            // 
-            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
-            // 
-            // bindingNavigatorPositionItem
-            // 
-            this.bindingNavigatorPositionItem.AccessibleName = "Положение";
-            this.bindingNavigatorPositionItem.AutoSize = false;
-            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
-            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
-            this.bindingNavigatorPositionItem.Text = "0";
-            this.bindingNavigatorPositionItem.ToolTipText = "Текущее положение";
-            // 
-            // bindingNavigatorCountItem
-            // 
-            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
-            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(43, 22);
-            this.bindingNavigatorCountItem.Text = "для {0}";
-            this.bindingNavigatorCountItem.ToolTipText = "Общее число элементов";
-            // 
-            // bindingNavigatorSeparator1
-            // 
-            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
-            // 
-            // bindingNavigatorMoveNextItem
-            // 
-            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
-            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
-            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorMoveNextItem.Text = "Переместить вперед";
-            // 
-            // bindingNavigatorMoveLastItem
-            // 
-            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
-            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
-            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorMoveLastItem.Text = "Переместить в конец";
-            // 
-            // bindingNavigatorSeparator2
-            // 
-            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
-            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
-            // 
-            // bindingNavigatorAddNewItem
-            // 
-            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
-            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
-            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorAddNewItem.Text = "Добавить";
-            // 
-            // bindingNavigatorDeleteItem
-            // 
-            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
-            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
-            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
-            this.bindingNavigatorDeleteItem.Text = "Удалить";
-            // 
-            // noteBindingNavigatorSaveItem
-            // 
-            this.noteBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.noteBindingNavigatorSaveItem.Enabled = false;
-            this.noteBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("noteBindingNavigatorSaveItem.Image")));
-            this.noteBindingNavigatorSaveItem.Name = "noteBindingNavigatorSaveItem";
-            this.noteBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 22);
-            this.noteBindingNavigatorSaveItem.Text = "Сохранить данные";
-            // 
             // dataGridViewNotes
             // 
-            this.dataGridViewNotes.AutoGenerateColumns = false;
+            this.dataGridViewNotes.AllowDrop = true;
             this.dataGridViewNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewNotes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewTextBoxColumn1,
             this.dataGridViewTextBoxColumn2});
-            this.dataGridViewNotes.DataSource = this.noteBindingSource;
-            this.dataGridViewNotes.Location = new System.Drawing.Point(11, 28);
+            this.dataGridViewNotes.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.dataGridViewNotes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewNotes.Location = new System.Drawing.Point(0, 0);
             this.dataGridViewNotes.Name = "dataGridViewNotes";
-            this.dataGridViewNotes.Size = new System.Drawing.Size(609, 452);
+            this.dataGridViewNotes.Size = new System.Drawing.Size(707, 544);
+            this.dataGridViewNotes.StandardTab = true;
             this.dataGridViewNotes.TabIndex = 3;
             // 
             // dataGridViewTextBoxColumn1
@@ -274,26 +136,39 @@ namespace Выведение_данных_в_datagridview
             this.dataGridViewTextBoxColumn2.HeaderText = "TextNote";
             this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
             // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(0, 0);
+            this.panel1.TabIndex = 4;
+            // 
+            // noteBindingSource
+            // 
+            this.noteBindingSource.DataSource = typeof(Выведение_данных_в_datagridview.Note);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 586);
-            this.Controls.Add(this.dataGridViewNotes);
-            this.Controls.Add(this.noteBindingNavigator);
+            this.AutoSize = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(707, 544);
             this.Controls.Add(this.buttonCreateNote);
+            this.Controls.Add(this.dataGridViewNotes);
+            this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Napominalka";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Resize += new System.EventHandler(this.Form1_Resize);
             this.contextMenuStrip1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.noteBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.noteBindingNavigator)).EndInit();
-            this.noteBindingNavigator.ResumeLayout(false);
-            this.noteBindingNavigator.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNotes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noteBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -307,23 +182,11 @@ namespace Выведение_данных_в_datagridview
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemOpen;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemClose;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemAutorun;
-        private System.Windows.Forms.BindingSource noteBindingSource;
-        private System.Windows.Forms.BindingNavigator noteBindingNavigator;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
-        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
-        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
-        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
-        private System.Windows.Forms.ToolStripButton noteBindingNavigatorSaveItem;
         private System.Windows.Forms.DataGridView dataGridViewNotes;
+        private System.Windows.Forms.BindingSource noteBindingSource;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
